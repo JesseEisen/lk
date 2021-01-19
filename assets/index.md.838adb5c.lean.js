1858557e82024ebb5b860b31bb97f6977672dc9e
Jesse Chan <lkchan0719@gmail.com>
2021-01-19 19:05:24 +0800

    feat: first deploy

---
+++ assets/index.md.838adb5c.lean.js
@@ -0,0 +1,1 @@
+import{l as t,f as e}from"./framework.2c0be2b9.js";const r='{"title":"Chan","description":"","frontmatter":{"title":"Chan","subtext":"GTD & PKM & Daily"},"relativePath":"index.md","lastUpdated":1610945460533}',a={};a.render=function(r,a,i,n,o,d){return t(),e("div")};export default a;export{r as __pageData};
